6409ae741666114f2dfaf96a691a17a155f47d97
Adam Podlaski <podlaski83@gmail.com>
2024-02-26 22:29:05 +0100

    initial commit

---
+++ src/main/java/com/vehicle_department/VehicleDepartmentApplication.java
@@ -0,0 +1,13 @@
+package com.vehicle_department;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleDepartmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleDepartmentApplication.class, args);
+    }
+
+}
