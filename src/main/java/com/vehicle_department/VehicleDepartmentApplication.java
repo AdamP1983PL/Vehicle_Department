f510282c30398b5122cc96fd8e1f51b8b3a9f7e8
Adam Podlaski <podlaski83@gmail.com>
2024-02-26 22:35:20 +0100

    mySql configuration in application.properties added

--- src/main/java/com/vehicle_department/VehicleDepartmentApplication.java
+++ src/main/java/com/vehicle_department/VehicleDepartmentApplication.java
@@ -2,8 +2,10 @@ package com.vehicle_department;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:database.properties")
 public class VehicleDepartmentApplication {
 
     public static void main(String[] args) {
